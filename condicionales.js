21c286544d57a0fe6a6bba20cc7caef398882b6b
JulianPaez92 <70548446+JulianPaez92@users.noreply.github.com>
2021-03-22 18:17:55 -0300

    Create condicionales.js

---
+++ condicionales.js
@@ -0,0 +1,49 @@
+console.log('Init Condicionales :D')
+
+// IF
+if (true){
+    // Code
+    
+    console.log('La condicion es verdadera')
+}
+
+if (1 === 1) {
+    console.log('La condicion es verd')
+}
+
+let numero = 1
+
+if ( numero === 1 ) {
+    console.log('verda')
+}
+
+// IF ELSE
+
+if(true){
+    console.log('es Verd')
+
+} else {
+    console.log('es Falsa')
+}
+
+//IF ELSE IF
+
+if(false) {
+    console.log('es verda')
+} else if(false){
+    console.log('es falsa')
+} else{
+    console.log('Todas fueron falsas')
+}
+
+//OPERADORES
+
+console.log( 1 == '1') //true
+console.log( 1 === '1') //false
+console.log( 1 != 1) //false
+console.log( 1 !== '1')
+//< ; <= ; <==
+//> ; >= ; >==
+
+console.log(true && true)
+console.log(true || true)
