4f88d224f72d49907c3e08816fafa85f5ab0ad55
JulianPaez92 <70548446+JulianPaez92@users.noreply.github.com>
2021-03-27 20:23:44 -0300

    Add files via upload

---
+++ js/12465PaezJulian.js
@@ -0,0 +1,5 @@
+let nombre = prompt("Ingresá tu nombre")
+let apellido = prompt("Ingresá tu apellido")
+let añoDeNacimiento = prompt("Ingresá tu año de nacimiento")
+let salida = "Tu nombre y apellido son: " + nombre + " " + apellido + ". Y tenés " + (2021 - añoDeNacimiento) + " años";
+alert(salida);
